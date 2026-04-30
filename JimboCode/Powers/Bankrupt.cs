86cfdaed0f8ec17b13534aafb05a89b36f6cb6c0
ThunderEdge <alvindong9913@gmail.com>
2026-04-29 21:23:20 -0500

    Implement Bankrupt

---
+++ JimboCode/Powers/Bankrupt.cs
@@ -0,0 +1,30 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Jimbo.JimboCode.Powers;
+
+public class Bankrupt : JimboPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target,
+        DamageResult result, ValueProp props,
+        Creature? dealer, CardModel? cardSource)
+    {
+        if (Owner.CombatState == null || target != Owner || result.UnblockedDamage <= 0 ||
+            Owner.CombatState.CurrentSide != Owner.Side)
+            return;
+        await PowerCmd.Remove(this);
+    }
+
+    public override async Task AfterRemoved(Creature oldOwner)
+    {
+        if (Owner.Player == null) return;
+        await PlayerCmd.LoseGold(Math.Min(Owner.Player.Gold, Amount), Owner.Player);
+    }
+}
